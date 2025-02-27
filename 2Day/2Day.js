f6e47848e420b928029ae286212ca41ae5761312
OctavioTrabajo <octaviopinoross@gmail.com>
2025-02-26 22:39:52 -0600

    Day 2 - Capture and store values within variables

--- 2Day/2Day.js
+++ 2Day/2Day.js
@@ -4,8 +4,9 @@ const lenguaje = prompt("¿Que lenguaje de programacion estas estudiando?");
 
 alert(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}`);
 const gusto = prompt(`¿Te gusta estudiar ${lenguaje}? Responde 1 para SÍ o  2 para NO.`);
-if(gusto == 1){
+
+if (gusto == 1){
     alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
-} else{
+} else {
     alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?")
 }
